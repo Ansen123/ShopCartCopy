66b03ed443a62d057ebb74adc95c4f9da27f14e1
Ansen123 <ansenfp@gmail.com>
2022-10-10 22:58:44 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Shoping'`, () => {
+  it(`should have as title 'ShopingCart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Shoping');
+    expect(app.title).toEqual('ShopingCart');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Shoping app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ShopingCart app is running!');
   });
 });
